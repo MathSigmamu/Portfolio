c8798c5d141fc5ee43453b292a7f2fb5033e5d4d
MathSigmamu <73785325+MathSigmamu@users.noreply.github.com>
2021-11-08 18:03:59 +0000

    Uploaded
    up

---
+++ LIGHT_files/fpc.pl
@@ -0,0 +1,5 @@
+// First Party Cookies
+// TS: 0 412 dc11_bf1
+
+window.ITTs[0].FPCV = 'ZQfNDAPj|4wjehXoPaa|fses10001231225479=|9HD0E0fPaa|ZQfNDAPj|fvis10001231225479=|8T1ss0M7HY|8T1ss0M7HY|8T1ss0M7HY|8s|8T1ss0M7HY|8T1ss0M7HY';
+window.ITTs[0].setFPCookies();
